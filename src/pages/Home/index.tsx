30b847b6e0d5085bda0c462d47d0855ae192303b
Rudnei Oliveira <rhoodsk8erboy@gmail.com>
2022-09-12 22:40:51 -0300

    create: creating new components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,9 +3,17 @@ import styles from "./styles.module.scss";
 export function Home() {
   return (
     <section className={styles.home_section}>
-      <header>
-        <h1>Welcome</h1>
-      </header>
+      <div className={styles.home_container}>
+        <h1>Welcome !</h1>
+        <div className={styles.home_content}>
+          <span>This is presentation application over websocket</span>
+          <span>
+            here will be shown how to navigate between chats and how to create
+            new chat rooms
+          </span>
+          <span>So let's talk</span>
+        </div>
+      </div>
     </section>
   );
 }
