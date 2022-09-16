ea75817f3230cded325787e82a7900d99a1c9978
Rudnei Oliveira <rhoodsk8erboy@gmail.com>
2022-09-15 23:03:05 -0300

    create: create list of chats

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -2,10 +2,29 @@ import React from "react";
 
 import styles from "./styles.module.scss";
 
+function ChatRoomCard() {
+  return (
+    <div className={styles.chat_room}>
+      <span>Chat Title</span>
+    </div>
+  );
+}
+
 export function Chat() {
   return (
     <section className={styles.chat_section}>
-      <h1>Chat</h1>
+      <header className={styles.chat_page_header}>
+        <h1>Chat's room</h1>
+        <h2>Select or create a chat room</h2>
+      </header>
+      <div className={styles.chat_info}>
+        <span>
+          Qtd of chats <b>(0)</b>
+        </span>
+      </div>
+      <div className={styles.chat_wrapper}>
+        <ChatRoomCard />
+      </div>
     </section>
   );
 }
